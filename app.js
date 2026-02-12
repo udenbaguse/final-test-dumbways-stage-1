69a6b2f498af14dc83b518323ac2c827ee8c782b
udenbaguse <udenbaguselord@gmail.com>
2026-02-13 03:55:54 +0700

    update dirname

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import hbs from "hbs";
 import path from "path";
-import { fileURLToPath } from "url";
+
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+const __dirname = import.meta.dirname
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
